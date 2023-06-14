5614d854b699b15cbceadfabe9cf7de7be49bf19
Mohammad Shaikh <ms18shai@siena.edu>
2023-06-14 15:01:16 -0400

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Shaikh, Mohammad on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
